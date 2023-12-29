e9cd43c3b91731b93eeb5f1cca29b1325b718083
XianScripts <christfontanez@gmail.com>
2023-12-29 09:26:24 -0500

    function working to reset books to add upon adding new book

--- Library.js
+++ Library.js
@@ -19,26 +19,85 @@ Book.prototype.printBookInfo = function() {
 
 
 
-let addBookButton = document.querySelector('.add-book');
-addBookButton.addEventListener('click', addBookToLibrary);
+// let addBookButton = document.querySelector('.add-book');
+// addBookButton.addEventListener('click', addBookToLibrary);
 
 
 // Creates new book and adds it to myLibrary array
+// let bookToAdd;
+// function addBookToLibrary() {
+//     let bookToAdd = new Book(prompt('Author?'), prompt('Title?'), parseInt(prompt('Pages?')), prompt('Read? True or False'));
+//     // parse true/false string
+//     switch(bookToAdd.read) {
+//         case 'True' || 'true': // lowercase not working
+//             bookToAdd.read = 'has been read!';
+//             break;
+//         case 'False' || 'false':
+//             bookToAdd.read = 'has not been read!';
+//             break;
+//     }
+//     myLibrary.push(bookToAdd);
+//     console.log(bookToAdd.printBookInfo());
+// }
+
+// Modal function to add Book
 let bookToAdd;
 function addBookToLibrary() {
-    let bookToAdd = new Book(prompt('Author?'), prompt('Title?'), parseInt(prompt('Pages?')), prompt('Read? True or False'));
-    // parse true/false string
-    switch(bookToAdd.read) {
-        case 'True' || 'true': // lowercase not working
-            bookToAdd.read = 'has been read!';
-            break;
-        case 'False' || 'false':
-            bookToAdd.read = 'has not been read!';
-            break;
-    }
+    console.log(bookToAdd);
+    let author = document.querySelector('.author').value;
+    let title = document.querySelector('.name').value;
+    let pages = document.querySelector('.pages').value;
+    let read = document.querySelector('.read').value;
+    bookToAdd = new Book(author, title, pages, read);
     myLibrary.push(bookToAdd);
-    console.log(bookToAdd.printBookInfo());
 }
+
+function resetBookButton() {
+    if (bookToAdd !== undefined) {
+        bookToAdd = undefined;
+    }
+    
+
+}
+
+const showButton = document.querySelector('#showDialog');
+const outputBox = document.querySelector('output');
+const confirmBtn = favDialog.querySelector('#confirmBtn');
+confirmBtn.addEventListener('click', () => {
+    addBookToLibrary()
+    resetBookButton();
+    favDialog.close();
+});
+
+
+
+// Upon clicking confirm button try to run function add book to library calling variables assigned
+// "Show the dialog" button opens the <dialog> modally
+showButton.addEventListener('click', () => {
+    favDialog.showModal();
+});
+
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Function to loop through array and display books
 function displayBooks() {
